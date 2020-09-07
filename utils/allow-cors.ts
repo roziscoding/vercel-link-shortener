a3bf86b4697337d72ede556acc81174d58555010
Rogério Munhoz <roz@rjmunhoz.me>
2020-09-06 23:22:50 -0300

    :lock: Fuck CORS

--- utils/allow-cors.ts
+++ utils/allow-cors.ts
@@ -6,6 +6,7 @@ export const allowCors: ApiHandlerWrapper = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader('Access-Control-Allow-Headers', '*')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
