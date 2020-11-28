3bbc1735f4315142474b536980a64e5149b353b5
Rogério Munhoz <roz@rjmunhoz.me>
2020-11-28 14:21:56 -0300

    :lock: Add auth to stats endpoint

--- api/stats.ts
+++ api/stats.ts
@@ -1,23 +1,26 @@
 import { getCountryList, getDailyVisits, getRefList } from '../services/statService'
 import { allowCors } from '../utils/allow-cors'
 import { extract } from '../utils/extract'
+import { requireAuth } from '../utils/require-auth'
 
-const shortcodeStats = allowCors(async (req, res) => {
-  const shortcode = extract('shortcode').from(req)
+const shortcodeStats = allowCors(
+  requireAuth(async (req, res) => {
+    const shortcode = extract('shortcode').from(req)
 
-  if (!shortcode) return res.end()
+    if (!shortcode) return res.end()
 
-  const [countries, refs, days] = await Promise.all([
-    getCountryList(shortcode),
-    getRefList(shortcode),
-    getDailyVisits(shortcode)
-  ])
+    const [countries, refs, days] = await Promise.all([
+      getCountryList(shortcode),
+      getRefList(shortcode),
+      getDailyVisits(shortcode)
+    ])
 
-  res.status(200).json({
-    countries,
-    refs,
-    days
+    res.status(200).json({
+      countries,
+      refs,
+      days
+    })
   })
-})
+)
 
 export default shortcodeStats
